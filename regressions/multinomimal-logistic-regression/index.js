699ae5fd3c250adfa8031ceceb2cfe737b84bd21
David Sims <dsims37@gatech.edu>
2019-03-25 12:50:26 -0700

    Logistical regression with mnist data set

--- regressions/multinomimal-logistic-regression/index.js
+++ regressions/multinomimal-logistic-regression/index.js
@@ -1,40 +1,32 @@
 require('@tensorflow/tfjs-node');
 const plot = require('node-remote-plot');
 const _ = require('lodash');
-const loadCSV = require('../data/load-csv');
 const LogisticRegression = require('./logistic-regression');
+const mnist = require('mnist-data');
 
-const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-        mpg: (value) => {
-            const mpg = parseFloat(value);
-
-            if (mpg < 15) {
-                return [1, 0, 0];
-            } else if (mpg < 30) {
-                return [0, 1, 0];
-            } else {
-                return [0, 0, 1];
-            }
-        }
-    },
-    dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['mpg']
+const mnistData = mnist.training(0, 5000);
+const features = mnistData.images.values.map(image => _.flatMap(image));
+const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
 });
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-    learningRate: 0.5,
-    iterations: 100,
-    batchSize: 10
+const regression = new LogisticRegression(features, encodedLabels, {
+    learningRate: 1,
+    iterations: 20,
+    batchSize: 100
 });
 
 regression.train();
 
-const test = regression.test(testFeatures, _.flatMap(testLabels));
-console.log(test);
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+});
 
-// plot({
-//     x: regression.costHistory.reverse()
-// });
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is', accuracy);
